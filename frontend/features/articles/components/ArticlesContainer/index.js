c8e8b7a21e55e01ea0475d7eacadd224f8d60e01
Derek Moore <derek@findthebrink.com>
2020-11-11 20:01:37 -0800

    feat(frontend): complete articles container base

--- frontend/features/articles/components/ArticlesContainer/index.js
+++ frontend/features/articles/components/ArticlesContainer/index.js
@@ -1,6 +1,79 @@
 // Libraries
 import React from "react";
+import { useQuery, gql } from '@apollo/client';
+import { DateTime } from 'luxon';
 
-const ArticlesContainer = () => <div>Display some articles here!</div>;
+const getArticlesListQuery = () => gql`
+  query GetArticles {
+    articles {
+      id
+      imageUrl
+      title
+      introduction
+      createdAt
+      author {
+        id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+const formatArticleDate = (date) => {
+  const dt = DateTime.fromISO(date);
+  return dt.toFormat('yyyy-mm-dd HH:mm');
+}
+
+const humanizeDate = (date) => {
+  const dt = DateTime.fromISO(date)
+  return dt.toLocaleString(DateTime.DATETIME_MED)
+}
+
+const ArticleItem = ({ id, imageUrl, title, introduction, createdAt, author }) => {
+  return (
+    <article role="contentinfo" aria-label="Article">
+      <header>
+        <h1>{title}</h1>
+        <div>
+          <p>{author.firstName} {author.lastName}</p>
+          <p>
+            <time dateTime={ formatArticleDate(createdAt)}>{ humanizeDate(createdAt) }</time>
+          </p>
+        </div>
+      </header>
+      <p>{introduction}</p>
+      <footer>
+        <a href={ `/article/${id}` } target="_blank">Read more</a>
+      </footer>
+    </article>
+  )
+}
+
+const ArticlesContainer = () => {
+  const { loading, error, data } = useQuery(getArticlesListQuery())
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+
+  return (
+    <ul>
+      {
+        data.articles.map(({ id, imageUrl, title, introduction, createdAt, author }) => (
+          <li key={id}>
+            <ArticleItem
+              id={id}
+              imageUrl={imageUrl}
+              title={title}
+              introduction={introduction}
+              createdAt={createdAt}
+              author={author}
+            />
+          </li>
+        ))
+      }
+    </ul>
+  )
+};
 
 export default ArticlesContainer;
